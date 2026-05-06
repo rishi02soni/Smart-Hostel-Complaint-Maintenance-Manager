c11d510d3fe1e7d0b2e45f764be2bfc1b1a57d4a
Rishi Soni <167216843+rishi02soni@users.noreply.github.com>
2026-05-06 14:10:10 +0530

    Create ElectricComplaint.java

---
+++ ElectricComplaint.java
@@ -0,0 +1,12 @@
+public class ElectricComplaint extends Complaint {
+
+    public ElectricComplaint(int id, String issue) {
+        super(id, issue);
+    }
+
+    @Override
+    public void displayComplaint() {
+        System.out.println("⚡ Electric Complaint");
+        super.displayComplaint();
+    }
+}
