323212d2f3898688c3f448448b8e676b8cceff13
Rishi Soni <167216843+rishi02soni@users.noreply.github.com>
2026-05-08 23:15:43 +0530

    Update Student.java

--- Student.java
+++ Student.java
@@ -3,7 +3,7 @@ public class Student extends User {
     public Student(String name, int id) {
         super(name, id);
     }
-
+ 
     public void raiseComplaint() {
         System.out.println(name + " raised a complaint.");
     }
