92f4a4be26d02bb907adcb72416b30ae1382c417
Rishi Soni <167216843+rishi02soni@users.noreply.github.com>
2026-05-08 23:14:41 +0530

    Update Admin.java

--- Admin.java
+++ Admin.java
@@ -3,7 +3,7 @@ public class Admin extends User {
     public Admin(String name, int id) {
         super(name, id);
     }
-
+ 
     public void solveComplaint(Complaint c) {
         c.setStatus("Resolved");
         System.out.println("Complaint Resolved by Admin.");
